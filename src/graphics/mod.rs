0cc6306eac84d4d120be06267537e85e95057650
adri326 <adrien.burgun@orange.fr>
2017-01-30 21:21:08 +0100

    Fixed some issues in the lighter() function

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -13,9 +13,9 @@ pub struct Color
 impl Color
 {
     /// Creates a new Color who is lighter than original.
-    pub fn lighter(&self) -> Color
+    pub fn lighter(&self, n: f32) -> Color
     {
-        let i: u8 = (1.0 / 0.3) as u8;
+        let i: u8 = (1.0 / (1.0 - n)) as u8;
         //let j: u8 = (255.0 / 0.7 - 255.0) as u8;
         let mut red = self.red;
         let mut green = self.green;
@@ -38,29 +38,29 @@ impl Color
                 blue = i;
             }
 
-        let mut lred: f32 = red as f32 / 0.7;
-        let mut lgreen: f32 = green as f32 / 0.7;
-        let mut lblue: f32 = blue as f32 / 0.7;
+        let mut lred: f32 = red as f32 / n;
+        let mut lgreen: f32 = green as f32 / n;
+        let mut lblue: f32 = blue as f32 / n;
 
         if lblue > 255.0
         {
-            let v = (blue as f32 / 0.7 - 255.0);
-            lgreen += v;
-            lred += v;
+            let v = (blue as f32 / n - 255.0);
+            if lgreen < v { lgreen += v; }
+            if lred < v { lred += v; }
         }
-
+        else
         if lgreen > 255.0
         {
-            let v = (green as f32 / 0.7 - 255.0);
-            lblue += v;
-            lred += v;
+            let v = (green as f32 / n - 255.0);
+            if lblue < v { lblue += v; }
+            if lred < v { lred += v; }
         }
-
+        else
         if lred > 255.0
         {
-            let v = (red as f32 / 0.7 - 255.0);
-            lgreen += v;
-            lblue += v;
+            let v = (red as f32 / n - 255.0);
+            if lgreen < v { lgreen += v; }
+            if lblue < v { lblue += v; }
         }
 
         blue = maths::min(lblue, 255.0) as u8;
@@ -101,6 +101,12 @@ pub static COLOR_RED: Color = Color { red: 255, green: 0, blue: 0, alpha: 255 };
 pub static COLOR_GREEN: Color = Color { red: 0, green: 255, blue: 0, alpha: 255 };
 /// The blue color.
 pub static COLOR_BLUE: Color = Color { red: 0, green: 0, blue: 255, alpha: 255 };
+/// The yellow color.
+pub static COLOR_YELLOW: Color = Color { red: 255, green: 255, blue: 0, alpha: 255 };
+/// The orange color.
+pub static COLOR_ORANGE: Color = Color { red: 255, green: 128, blue: 0, alpha: 255 };
+/// The cyan color.
+pub static COLOR_CYAN: Color = Color { red: 0, green: 255, blue: 255, alpha: 255 };
 
 pub struct IonicGraphics
 {
